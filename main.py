d7daf833d32b8c9b42b68143820fe2847e385c82
haeffema <haeffema@gmail.com>
2021-06-30 21:27:19 +0200

    anfang

---
+++ main.py
@@ -0,0 +1,4 @@
+# die main immer ausführen, dann muss man seinen Interpreter nicht ändern, einfach hier die datei ausführen aber main nicht pushen ;)
+# wobei des kann man auch ez ändern, also wie ihr es wollt :)
+
+import pokcalc
---
+++ pokcalc.py
@@ -0,0 +1,1 @@
+# ganz early phase :)
