64b3224cf6b0931c2735e4e2fd6b8120f3802e98
haeffema <haeffema@gmail.com>
2021-07-01 09:06:06 +0200

    lolol

--- Pokemon.py
+++ Pokemon.py
@@ -1,12 +1,23 @@
 import Type
+
+
 class Stats(object):
-    def __init__(self, hp, ):
+    def __init__(self, hp, physattack, physdef, spezattack, spezdef, speed):
         self.hp = hp
+        self.attack = physattack
+        self.physdef = physdef
+        self.spezattack = spezattack
+        self.spezdef = spezdef
+        self.speed = speed
+
 
 class Pokemon(object):
-    def __init__(self, name, firstType, secondType):
+    def __init__(self, name, firstType, secondType, stats):
         self.name = name
         self.firstType = firstType
         self.secondType = secondType
+        self.stats = stats
 
-Gengar = Pokemon("Gengar", Type.Ghost, Type.Poison)
+
+GengarBase = (60, 65, 60, 130, 75, 110)
+Gengar = Pokemon("Gengar", Type.Ghost, Type.Poison, GengarBase)
