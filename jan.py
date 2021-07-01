eedd2f9e3833a80a8c1031b10d5e40b44a8af830
JanDerZerd <jan.stoerk@gmail.com>
2021-07-01 08:30:26 +0200

    Reinigungscode

--- jan.py
+++ jan.py
@@ -2,5 +2,12 @@ print('info is super*')
 print("pokemon Team")
 pokemon = "Marshadow"
 print(pokemon)
-i=input("message die du sagen willst ")
-print(i+i+i+i+i)
+i=input("message die du sagen willst \n")
+maxstinkt=True
+duschen=0
+while maxstinkt is True:
+    print(i+"\n")
+    duschen=duschen+1
+    if duschen == 10:
+        maxstinkt = False
+        print("stinkt jetzt nicht mehr")
