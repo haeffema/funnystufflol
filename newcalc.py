d5268a2da5188c411b10a23062ea0233a51aae50
haeffema <52890607+haeffema@users.noreply.github.com>
2021-07-01 11:58:09 +0200

    newcalc.py
    xdxdxdxd

---
+++ newcalc.py
@@ -0,0 +1,11 @@
+attackstat = 479
+defstat = 171
+hp = 285
+stab = 1.5
+attackdmg = 120
+effectiv = 2
+damagemax = ((42 * attackdmg * attackstat/defstat)/50)*stab*effectiv
+damagemin = damagemax*0.85
+percent = round(damagemax/hp * 100, 2)
+percentmin = round(damagemin/hp * 100, 2)
+print("The Attack will do dmg between " + str(percentmin)+ "% and " + str(percent) + "% damage")
