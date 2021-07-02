bbdad70971c4dab14df1bafe0a00375a655e59b6
haeffema <52890607+haeffema@users.noreply.github.com>
2021-07-02 11:39:13 +0200

    added a new method

--- Type.py
+++ Type.py
@@ -1,32 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 class Type(object):
-    def __init__(self, name):
-        self.name = name
 
     weaknesses = []
     resistances = []
-    immunities = [None]
+    immunities = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def calculateEffectivity(self, type):
+        effect = 1
+        for z in self.immunities:
+            if z == type:
+                effect = 0
+        for x in self.weaknesses:
+            if x == type:
+                effect = 2
+        for y in self.resistances:
+            if y == type:
+                effect = 0.5
+        return effect
 
 
-Normal = Type("Normal")
-Fighting = Type("Fighting")
-Flying = Type("Flying")
-Poison = Type("Poison")
-Ground = Type("Ground")
-Rock = Type("Rock")
-Bug = Type("Bug")
-Ghost = Type("Ghost")
-Steel = Type("Steel")
-Fire = Type("Fire")
-Water = Type("Water")
-Grass = Type("Grass")
-Electric = Type("Electric")
-Psychic = Type("Psychic")
-Ice = Type("Ice")
-Dragon = Type("Dragon")
-Dark = Type("Dark")
-Fairy = Type("Fairy")
+Normal = Type('Normal')
+Fighting = Type('Fighting')
+Flying = Type('Flying')
+Poison = Type('Poison')
+Ground = Type('Ground')
+Rock = Type('Rock')
+Bug = Type('Bug')
+Ghost = Type('Ghost')
+Steel = Type('Steel')
+Fire = Type('Fire')
+Water = Type('Water')
+Grass = Type('Grass')
+Electric = Type('Electric')
+Psychic = Type('Psychic')
+Ice = Type('Ice')
+Dragon = Type('Dragon')
+Dark = Type('Dark')
+Fairy = Type('Fairy')
 
 # Weaknesses:
+
 Normal.weaknesses = [Fighting]
 Fighting.weaknesses = [Flying, Psychic, Fairy]
 Flying.weaknesses = [Rock, Electric, Ice]
@@ -44,8 +63,6 @@ Ice.weaknesses = [Fighting, Rock, Steel, Fire]
 Dragon.weaknesses = [Ice, Dragon, Fairy]
 Dark.weaknesses = [Fighting, Bug, Fairy]
 Fairy.weaknesses = [Poison, Steel]
-for x in Rock.weaknesses:
-    print(x.name)
 
 # Resistances:
 
@@ -54,10 +71,21 @@ Fighting.resistances = [Rock, Bug, Dark]
 Flying.resistances = [Fighting, Bug, Grass]
 Poison.resistences = [Fighting, Poison, Bug, Grass, Fairy]
 Ground.resistences = [Poison, Rock]
-Rock.resistences = [Normal, Flying, Poison, Fire] 
+Rock.resistences = [Normal, Flying, Poison, Fire]
 Bug.resistences = [Fighting, Ground, Grass]
 Ghost.resistences = [Poison, Bug]
-Steel.resistences = [Normal, Flying, Rock, Bug, Steel, Grass, Psychic, Ice, Dragon, Fairy]
+Steel.resistences = [
+    Normal,
+    Flying,
+    Rock,
+    Bug,
+    Steel,
+    Grass,
+    Psychic,
+    Ice,
+    Dragon,
+    Fairy,
+    ]
 Fire.resistences = [Bug, Steel, Fire, Ice, Fairy]
 Water.resistences = [Steel, Fire, Water, Ice]
 Grass.resistences = [Ground, Water, Grass, Electric]
@@ -68,12 +96,15 @@ Dark.resistences = [Ghost, Dark]
 Fairy.resistences = [Fighting, Bug, Dark]
 
 # Immunities:
-Normal.immunities = [Ghost]
-Flying.immunities=[Ground]
-Ground.immunities=[Electric]
-Ghost.immunities=[Normal,Fighting]
-Steel.immunities=[Poison]
-Dark.immunities=[Psychic]
-Fairy.immunities =[Dragon]
 
-#jo jani du zerdi lul man kann des alles in ner txt machen und einlesen lassen, wenn du willst mach ich des schreib halt auf dc hihi
+Normal.immunities = [Ghost]
+Flying.immunities = [Ground]
+Ground.immunities = [Electric]
+Ghost.immunities = [Normal, Fighting]
+Steel.immunities = [Poison]
+Dark.immunities = [Psychic]
+Fairy.immunities = [Dragon]
+
+print (Fairy.calculateEffectivity(Ghost))
+
+# jo jani du zerdi lul man kann des alles in ner txt machen und einlesen lassen, wenn du willst mach ich des schreib halt auf dc hihi
