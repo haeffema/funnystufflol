4e335fdca12424a2c10b456972b1aba657969e2f
haeffema <haeffema@gmail.com>
2021-07-02 15:01:15 +0200

    creating new files -> clean code

--- implementation.py
+++ implementation.py
@@ -1,3 +1,6 @@
 import Pokemon
-import Stats
+import stats
 import Attack
+
+GengarBase = stats.Stats(60, 65, 60, 130, 75, 110)
+Gengar = Pokemon("Gengar", Type.Ghost, Type.Poison, GengarBase)
--- pokemon.py
+++ pokemon.py
@@ -1,12 +1,3 @@
-import Type
-import Attack
-import Stats
-
-spez = "Special"
-phys = "Physical"
-status = "Status"
-
-
 class Pokemon(object):
     def __init__(self, name, firstType, secondType, stats):
         self.name = name
@@ -18,9 +9,3 @@ class Pokemon(object):
         effect = self.firstType.calculateEffectivity(type)
         effect2 = self.secondType.calculateEffectivity(type)
         return effect * effect2
-
-
-GengarBase = Stats(60, 65, 60, 130, 75, 110)
-Gengar = Pokemon("Gengar", Type.Ghost, Type.Poison, GengarBase)
-
-print(Gengar.calculateEffecticityForPokemon(Type.Ground))
