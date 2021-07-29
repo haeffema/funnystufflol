c8614f91cc3bcce18c4a1a55fd2cf04cdec88a4f
haeffema <haeffema@gmail.com>
2021-07-29 23:23:44 +0200

    prob 9

--- ProjectEulerMax.py
+++ ProjectEulerMax.py
@@ -104,3 +104,12 @@ def prob8():
 
 
 def prob9():
+    for a in range(1, 1000):
+        for b in range(1, 1000):
+            for c in range(1, 1000):
+                if a * a + b * b == c * c and a + b + c == 1000:
+                    if a * b * c > 0:
+                        print(a * b * c)
+                        a = 1000
+                        b = 1000
+                        c = 1000
