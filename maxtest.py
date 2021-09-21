4c7182ea9e233807546e13d7e5eb4faf13f83a1e
haeffema <haeffema@gmail.com>
2021-09-21 13:59:50 +0200

    aydf

--- maxtest.py
+++ maxtest.py
@@ -1,6 +1,11 @@
 datei = open('pokemon.txt', 'r', encoding='UTF-8')
 Text = ""
+counter = 0
 for zeile in datei:
+    if counter == 3:
+        zeile = zeile.replace("\n", "")
+        counter == 0
+    counter += 1
     Text += zeile
 datei.close()
 print(Text)
